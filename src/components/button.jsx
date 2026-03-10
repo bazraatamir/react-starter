7b88331e4fe12228badeddcc8ff7d5dc08f946e9
bazraatamir <69583471+bazraatamir@users.noreply.github.com>
2026-03-10 10:42:00 +0800

    starter

---
+++ src/components/button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./button.css"
+
+function Button(){
+    return (
+        <div>
+            <button className="btn">click</button>
+        </div>
+    )
+}
+
+export default Button
